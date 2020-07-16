4414b4cac42d75ad01773448110cf003d7acf7b1
kdbrill <brik8110@chsphantoms.org>
2020-07-16 17:53:46 +0000

    command method

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -13,7 +13,7 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return new ShootCommand(shooter);
-        return null;
+        return new ShootCommand(shooter);command.withTimout
+ 
     }
 }
